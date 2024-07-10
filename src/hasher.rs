f91d54780edba93e01d21dcd2c0c215f2189f8aa
milosdjurica <milosdjurica13@gmail.com>
2024-07-10 20:48:14 +0200

    created a hasher

---
+++ src/hasher.rs
@@ -0,0 +1,24 @@
+use tiny_keccak::{Hasher, Keccak};
+
+pub struct FastHasher {
+    hash_engine: Keccak,
+}
+
+impl FastHasher {
+    pub fn new() -> Self {
+        FastHasher {
+            hash_engine: Keccak::v256(),
+        }
+    }
+
+    pub fn add_to_hash(&mut self, input_data: &[u8]) -> Result<(), std::io::Error> {
+        self.hash_engine.update(input_data);
+        Ok(())
+    }
+
+    pub fn get_hash(&mut self) -> Vec<u8> {
+        let mut result = [0u8; 32];
+        self.hash_engine.clone().finalize(&mut result);
+        result.to_vec()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod hasher;
+
+use hasher::FastHasher;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut hasher = FastHasher::new();
+
+    let data = b"Hello, world!";
+
+    hasher
+        .add_to_hash(data)
+        .expect("Failed to add data to hash");
+
+    let hash_result = hasher.get_hash();
+    println!("Hash result {:#?}", hash_result);
 }
