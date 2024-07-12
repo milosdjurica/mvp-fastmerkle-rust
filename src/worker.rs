f8a92ad9a104299958a19e9f01099d014b7c608c
milosdjurica <milosdjurica13@gmail.com>
2024-07-12 16:14:13 +0200

    WorkerPool new get_results and close methods

--- src/worker.rs
+++ src/worker.rs
@@ -1,4 +1,4 @@
-use crossbeam_channel::{Receiver, Sender};
+use crossbeam_channel::{bounded, Receiver, Sender};
 
 pub struct WorkerPool {
     results_sender: Sender<WorkerResult>,
@@ -16,4 +16,21 @@ pub struct WorkerResult {
     error: Option<Box<dyn std::error::Error + Send + Sync>>,
 }
 
-impl WorkerPool {}
+impl WorkerPool {
+    pub fn new(expected_num_results: usize) -> Self {
+        let (sender, receiver) = bounded(expected_num_results);
+        WorkerPool {
+            results_sender: sender,
+            results_receiver: receiver,
+        }
+    }
+
+    pub fn get_result(&self) -> WorkerResult {
+        self.results_receiver.recv().unwrap()
+    }
+
+    pub fn close(&self) {
+        drop(&self.results_sender);
+        drop(&self.results_receiver);
+    }
+}
