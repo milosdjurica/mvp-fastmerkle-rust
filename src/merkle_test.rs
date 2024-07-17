3e54e7f0c5d088dfec34e98052057fc530459282
milosdjurica <milosdjurica13@gmail.com>
2024-07-17 11:54:52 +0200

    started writting tests

---
+++ src/merkle_test.rs
@@ -0,0 +1,14 @@
+fn benchmark_generate_merkle_tree_5() {}
+fn benchmark_generate_merkle_tree_50() {}
+fn benchmark_generate_merkle_tree_500() {}
+fn benchmark_generate_merkle_tree_1_000() {}
+fn benchmark_generate_merkle_tree_10_000() {}
+fn benchmark_generate_merkle_tree_1_000_000() {}
+
+fn generate_random_data() {}
+
+fn generate_input_set() {}
+
+fn get_hex_bytes() {}
+
+fn test_generate_merkle_tree() {}
