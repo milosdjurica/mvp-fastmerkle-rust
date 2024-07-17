46ec7b0422b1a55e1e43a78b4c5b1cc3f469bc8a
milosdjurica <milosdjurica13@gmail.com>
2024-07-17 13:40:20 +0200

    generate_input_set()

--- src/merkle_test.rs
+++ src/merkle_test.rs
@@ -10,7 +10,23 @@ fn benchmark_generate_merkle_tree_1_000_000() {}
 
 fn generate_random_data(size: usize) -> Vec<Vec<u8>> {}
 
-fn generate_input_set() {}
+fn generate_input_set(size: usize) -> Vec<Vec<u8>> {
+    let input_set = vec![
+        b"Lazar".to_vec(),
+        b"Vuksan".to_vec(),
+        b"Dusan".to_vec(),
+        b"Aleksa".to_vec(),
+        b"Yoshiki".to_vec(),
+        b"Milos".to_vec(),
+        b"Zeljko".to_vec(),
+    ];
+
+    if size > input_set.len() {
+        input_set.clone()
+    } else {
+        input_set[..size].to_vec()
+    }
+}
 
 fn get_hex_bytes(input: &str) -> Result<Vec<u8>, Box<dyn Error>> {
     let hex_bytes = hex::decode(input).unwrap();
