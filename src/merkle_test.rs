3f54a6e5a8f809d62eb5d537d8f8974852a6fe32
milosdjurica <milosdjurica13@gmail.com>
2024-07-17 13:01:07 +0200

    hex package and get_hex_bytes() function

--- src/merkle_test.rs
+++ src/merkle_test.rs
@@ -1,3 +1,6 @@
+use hex;
+use std::error::Error;
+
 fn benchmark_generate_merkle_tree_5() {}
 fn benchmark_generate_merkle_tree_50() {}
 fn benchmark_generate_merkle_tree_500() {}
@@ -5,10 +8,13 @@ fn benchmark_generate_merkle_tree_1_000() {}
 fn benchmark_generate_merkle_tree_10_000() {}
 fn benchmark_generate_merkle_tree_1_000_000() {}
 
-fn generate_random_data() {}
+fn generate_random_data(size: usize) -> Vec<Vec<u8>> {}
 
 fn generate_input_set() {}
 
-fn get_hex_bytes() {}
+fn get_hex_bytes(input: &str) -> Result<Vec<u8>, Box<dyn Error>> {
+    let hex_bytes = hex::decode(input).unwrap();
+    Ok(hex_bytes)
+}
 
 fn test_generate_merkle_tree() {}
